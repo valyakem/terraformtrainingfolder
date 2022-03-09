e982db8321f16a3a02d96affbecb1aef47176959
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-09 14:52:42 -0500

    added some stuff

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "nb_vpc01" {
     source        = "modules/vpc"
     vpc_cidr      = "192.168.0.0/16"
     tenancy       = "default"
-    vpc_id        = "${module.vpc.vpc_id}"
+    vpc_id        = "${module.nb_vpc01.vpc_id}"
     subnet_cidr   = "192.168.1.0/24"
 }
 
@@ -10,5 +10,5 @@ module "nb_ec2" {
     source = "modules/ec2"
     ec2_count       = 1
     instance_type   = "t2.micro"
-    subnet_id       = "${module.vpc.subnet_id}"
+    subnet_id       = "${module.nb_ec2.subnet_id}"
 }
