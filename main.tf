b563048de12779966f2164aa3ef823e9a340f115
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-09 16:17:44 -0500

    added some files

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "nb_vpc01" {
-    source        = "../../modules/vpc"
+    source        = "modules/vpc"
     vpc_cidr      = "192.168.0.0/16"
     tenancy       = "default"
     vpc_id        = "${module.nb_vpc01.vpc_id}"
@@ -7,7 +7,7 @@ module "nb_vpc01" {
 }
 
 module "nb_ec2" {
-    source = "../../modules/ec2"
+    source = "modules/ec2"
     ec2_count       = 1
     instance_type   = "t2.micro"
     subnet_id       = "${module.nb_ec2.subnet_id}"
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.7.0"
+    }
+  }
+}
+
 resource "aws_instance" "web" {
   count         = "${var.ec2_count}"
   ami           = "${var.ami_id}"
