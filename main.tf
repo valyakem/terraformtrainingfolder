d9efb54859508683b500240608a22dd60367ef7b
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-09 17:00:43 -0500

    We added some files

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "nbvpc" {
     source        = "./modules/vpc"
     vpc_cidr      = "192.168.0.0/16"
     tenancy       = "default"
-    vpc_id        = "${module.nbvpc.vpc_id.id}"
+    vpc_id        = "${module.nbvpc.vpc_id}"
     subnet_cidr   = "192.168.1.0/24"
 }
 
@@ -10,5 +10,5 @@ module "nbec2" {
     source = "./modules/ec2"
     ec2_count       = 1
     instance_type   = "t2.micro"
-    subnet_id       = "${module.nbec2.subnet_id.id}"
+    subnet_id       = "${module.nbec2.subnet_id}"
 }
