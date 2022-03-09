158a9a841ea16978fd381480183ae5ede90188d4
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-09 13:28:33 -0500

    Added some files

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ resource "aws_vpc" "main" {
   cidr_block        = "10.10.0.0/16"
 }
 
-module "nb-webserver" {
+module "nbwebserver" {
   source            = "modules/webserver"
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.10.0.0/16"
-  webserver_name    = "nb"
+  webserver_name    = "nb-webserver"
   ami               = "ami-04505e74c0741db8d"
   instance_type     = "t2.micro"
 }
